29fbf0731e9f59ef5014ff0b30acc773036fed9a
Srinidhi Srinivasan <srsrinivasan@hmc.edu>
2016-10-20 18:28:45 -0500

    created all tsv data files for every character of every play. Done by automation. Nothing done manually

--- scripts/lineDensityParser.py
+++ scripts/lineDensityParser.py
@@ -29,143 +29,161 @@ filenames = {
     "win_tale": "WT"
 }
 
-# for filename in sys.argv: 
-# 	if filename == "lineDensityParser.py":
-# 		continue
-# 	slashIndex = filename.index("/")
-# 	extensionIndex = filename.index(".xml")
-# 	file = filename[slashIndex + 1: extensionIndex - 1]
-# 	filename = filenames[file]
 
-filename = "testing.xml"
-extensionIndex = filename.index(".xml")
-directory = filename[:extensionIndex]
+directory = ""
+for play in filenames:
+	relpath = "../xml/" + play + ".xml"
+	filename = os.path.join(os.path.dirname(__file__), relpath)
+	print "filename", filename
+	slashIndex = filename.index("/")
+	filenameNew = filename[slashIndex + 1:]
+	slashIndex2 = filenameNew.index("/")
+	extensionIndex = filenameNew.index(".xml")
+	filenameNew2 = filenameNew[slashIndex2 + 1: extensionIndex]
+	print "filenameNew2", filenameNew2
+	directory = filenames[filenameNew2]
 
-e = xml.etree.ElementTree.parse(filename).getroot()
+	# for filename in sys.argv: 
+	# 	if filename == "lineDensityParser.py":
+	# 		continue
+	# 	print "filename", filename
+	# 	slashIndex = filename.index("/")
+	# 	extensionIndex = filename.index(".xml")
+	# 	file = filename[slashIndex + 1: extensionIndex - 1]
+	# 	directory = filenames[file]
 
-#chars: list of all characters;
-#sceneChars: 2d list of characters: 2d list of character per scene. 
+	# filename = "macbeth.xml"
+	# extensionIndex = filename.index(".xml")
+	# directory = filename[:extensionIndex]
 
-#what i want is a 2d array where the rows are all the characters and the
-#columns are tuples with the scene number and the number of lines. 
+	e = xml.etree.ElementTree.parse(filename).getroot()
 
-#sceneChars = [[]]
+	#chars: list of all characters;
+	#sceneChars: 2d list of characters: 2d list of character per scene. 
 
-chars = []
-actCount = 0;
-playFormat = []
-charsInScene = {} #dictionary
-allCharsLines = []
-index = 0
+	#what i want is a 2d array where the rows are all the characters and the
+	#columns are tuples with the scene number and the number of lines. 
+
+	#sceneChars = [[]]
+
+	chars = []
+	actCount = 0;
+	playFormat = []
+	charsInScene = {} #dictionary
+	allCharsLines = []
+	index = 0
 
 
-for act in e.iter("ACT"):
-	actCount += 1
-	sceneCount = 1
-	for scene in act.iter("SCENE"):
-		sceneLines = {}
-		playFormat += [[actCount, sceneCount]]
-		#print "playFormat", playFormat
-		for speech in scene.iter("SPEECH"):
-			for speaker in speech.iter("SPEAKER"):
+	for act in e.iter("ACT"):
+		actCount += 1
+		sceneCount = 1
+		for scene in act.iter("SCENE"):
+			sceneLines = {}
+			playFormat += [[actCount, sceneCount]]
+			#print "playFormat", playFormat
+			for speech in scene.iter("SPEECH"):
+				for speaker in speech.iter("SPEAKER"):
 
-				if speaker.text not in chars:
-					chars += [speaker.text]
-				lineCount = 0
-				for line in speech.iter("LINE"):
-					lineCount += 1
-					if (speaker.text in sceneLines):
-						sceneLines[speaker.text] += (lineCount+1)/2
-					else:
-						sceneLines[speaker.text] = (lineCount+1)/2 
+					if speaker.text not in chars:
+						chars += [speaker.text]
 					lineCount = 0
-		print "sceneLines with scene: ", sceneCount, sceneLines
-		allCharsLines += [sceneLines]
-		sceneCount += 1
+					for line in speech.iter("LINE"):
+						lineCount += 1
+						if (speaker.text in sceneLines):
+							sceneLines[speaker.text] += (lineCount+1)/2
+						else:
+							sceneLines[speaker.text] = (lineCount+1)/2 
+						lineCount = 0
+			print "sceneLines with scene: ", sceneCount, sceneLines
+			allCharsLines += [sceneLines]
+			sceneCount += 1
 
-	
+		
 
-#print "sceneLines: ", sceneLines
-print "playFormat: ", playFormat
-print "allCharsLines: ", allCharsLines
-print "characters: ", chars
+	#print "sceneLines: ", sceneLines
+	print "playFormat: ", playFormat
+	print "allCharsLines: ", allCharsLines
+	print "characters: ", chars
 
-actAndScene = [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2]]
-characters = [{'First Witch': 2, 'Fifth Witch': 1}, {'Second Witch': 4}, {'Third Witch': 6}, {'First Witch': 1}, {'Second Witch': 3}]
-chars = ['First Witch', 'Second Witch', 'Third Witch', 'Fifth Witch']
-#want to be in format: character = [("Act1_S1", # of lines), ("Act1_S2", # of lines)]
+	#actAndScene = [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2]]
+	#characters = [{'First Witch': 2, 'Fifth Witch': 1}, {'Second Witch': 4}, {'Third Witch': 6}, {'First Witch': 1}, {'Second Witch': 3}]
+	#chars = ['First Witch', 'Second Witch', 'Third Witch', 'Fifth Witch']
+	#want to be in format: character = [("Act1_S1", # of lines), ("Act1_S2", # of lines)]
 
-totalScenes = len(actAndScene)
-totalChars = len(chars)
-print "totalScenes", totalScenes
-print "totalChars", totalChars
-characterLines = [[]]*totalChars
-print characterLines
+	totalScenes = len(playFormat)
+	totalChars = len(chars)
+	print "totalScenes", totalScenes
+	print "totalChars", totalChars
+	characterLines = [[]]*totalChars
+	print characterLines
 
-#for character in chars:
-#for i in range(totalScenes):
+	#for character in chars:
+	#for i in range(totalScenes):
 
-#create directory for each play
-path = "../src/visualizations/tsvDat/"
+	#create directory for each play
+	path = "../src/visualizations/tsvDat/"
 
-if not os.path.isdir(path + directory):
-	os.mkdir(path + directory, 0755)
+	if not os.path.isdir(path + directory):
+		os.mkdir(path + directory, 0755)
 
-counterForIndex = -1
-for scene in characters: #also get index of this and should be good
-	counterForIndex += 1
-	for key in scene:
-		print "key", key
-		if key in chars:
-			print "actAndScene", actAndScene[counterForIndex]
-			whichScene = "Act" + str(actAndScene[counterForIndex][0]) + "_S" + str(actAndScene[counterForIndex][1])
-			index = chars.index(key)
-			print "index", index
-			if characterLines[index] == []:
-				characterLines[index] = [whichScene, scene[key]]
-			else:
-				characterLines[index] += [whichScene, scene[key]]
-			print "characterLines", characterLines
-	#counterForIndex += 1
+	counterForIndex = -1
+	for scene in allCharsLines: #also get index of this and should be good
+		counterForIndex += 1
+		for key in scene:
+			print "key", key
+			if key in chars:
+				print "actAndScene", playFormat[counterForIndex]
+				whichScene = "Act" + str(playFormat[counterForIndex][0]) + "_S" + str(playFormat[counterForIndex][1])
+				index = chars.index(key)
+				print "index", index
+				if characterLines[index] == []:
+					characterLines[index] = [whichScene, scene[key]]
+				else:
+					characterLines[index] += [whichScene, scene[key]]
+				print "characterLines", characterLines
+		#counterForIndex += 1
 
-print "hopefuly correct", characterLines
+	print "hopefuly correct", characterLines
 
-# answer should be: 
-# [
-# 	[([1,1], 2), ([2,1], 1)],
-# 	[([1,2], 4), ([2,2], 3)],
-# 	[([1,3], 6)], 
-# 	[([1,1], 1)]
-# ]
+	# answer should be: 
+	# [
+	# 	[([1,1], 2), ([2,1], 1)],
+	# 	[([1,2], 4), ([2,2], 3)],
+	# 	[([1,3], 6)], 
+	# 	[([1,1], 1)]
+	# ]
 
-#create all the tsv files for each play:
+	#create all the tsv files for each play:
 
-def deleteContent(fileToDelete):
-	fileToDelete.seek(0)
-	fileToDelete.truncate()
+	# def deleteContent(fileToDelete):
+	# 	fileToDelete.seek(0)
+	# 	fileToDelete.truncate()
 
 
 
-print directory
+	print directory
 
-#print file
-print "------------------------"
+	#print file
+	print "------------------------"
 
-#generates files for each character of a play and puts it in folder for that play
-characterLines = [['Act1_S1', 2, 'Act2_S1', 1], ['Act1_S2', 4, 'Act2_S2', 3], ['Act1_S3', 6], ['Act1_S1', 1]]
-chars = ['First Witch', 'Second Witch', 'Third Witch', 'Fifth Witch']
-for numLines in range(len(characterLines)):
-	tsv = ""
-	print "numLines", numLines
-	for i in range(0, len(characterLines[numLines]) - 1, 2):
-		print i
-		tsv += str(characterLines[numLines][i])
-		tsv += "\t" + str(characterLines[numLines][i+1])
-		tsv += "\n"
-	print "character: ", numLines, "lines: ", tsv
-	charFilename = chars[numLines].replace(" ", "")
-	f = open("../src/visualizations/tsvDat/" + directory + "/" + charFilename + ".tsv", 'w+')
-	f.write(tsv)
+	#generates files for each character of a play and puts it in folder for that play
+	#characterLines = [['Act1_S1', 2, 'Act2_S1', 1], ['Act1_S2', 4, 'Act2_S2', 3], ['Act1_S3', 6], ['Act1_S1', 1]]
+	#chars = ['First Witch', 'Second Witch', 'Third Witch', 'Fifth Witch']
+	for numLines in range(len(characterLines)):
+		tsv = ""
+		print "numLines", numLines
+		for i in range(0, len(characterLines[numLines]) - 1, 2):
+			print i
+			tsv += str(characterLines[numLines][i])
+			tsv += "\t" + str(characterLines[numLines][i+1])
+			tsv += "\n"
+		print "character: ", numLines, "lines: ", tsv
+		charFilename = chars[numLines].replace(" ", "")
+		f = open("../src/visualizations/tsvDat/" + directory + "/" + charFilename + ".tsv", 'w+')
+		f.write(tsv)
+
+
+
 
 
 
